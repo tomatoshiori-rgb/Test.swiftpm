130d6d56c169f81fe309b5c46e072d78f63fa4f0
tomatoshiori-rgb <tomatoshiori@icloud.com>
2026-03-08 22:48:20 +0900

    Update iOS version and bundle identifier in Package.swift

--- Package.swift
+++ Package.swift
@@ -5,19 +5,24 @@ import AppleProductTypes
 let package = Package(
     name: "TestApp",
     platforms: [
-        .iOS("26.0") // 最新OSに合わせる
+        .iOS("18.0") // ここを 26.0 から 18.0 に修正！
     ],
     products: [
         .iOSApplication(
             name: "TestApp",
             targets: ["AppModule"],
-            bundleIdentifier: "com.user.testapp.v26", // 既存アプリと被らない名前に変更
+            bundleIdentifier: "com.user.testapp.v18",
             displayVersion: "1.0",
             bundleVersion: "1",
             appIcon: .placeholder(icon: .heart),
             accentColor: .presetColor(.blue),
             supportedDeviceFamilies: [.pad, .phone],
-            supportedInterfaceOrientations: [.portrait, .landscapeLeft, .landscapeRight]
+            supportedInterfaceOrientations: [
+                .portrait,
+                .landscapeLeft,
+                .landscapeRight,
+                .portraitUpsideDown // 警告対応：全ての向きを許可
+            ]
         )
     ],
     targets: [
